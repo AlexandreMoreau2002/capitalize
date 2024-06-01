3cdd432616ce7d07143d0552fc3bf4620686b821
Alexandre <moreaualexandre2002@gmail.com>
2024-06-01 22:16:44 +0200

    refactor: showNotification

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ document.querySelector(".copy").addEventListener("click", function () {
   const resultText = document.querySelector(".result").textContent;
   navigator.clipboard.writeText(resultText).then(
     () => {
-      // showNotification("Texte copié dans le presse-papier");
-      showNotification("La licorne est bien arrivée");
+      showNotification("Texte copié dans le presse-papier");
     },
     () => {
       showNotification("Échec de la copie du texte");
